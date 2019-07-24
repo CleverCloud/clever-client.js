bf6fa9188ea22126182282bc173fc8851a5f749c
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2019-07-24 19:21:52 +0200

    Fix JSON handling for node (request.request.js)

--- esm/request.request.js
+++ esm/request.request.js
@@ -1,28 +1,66 @@
 import req from 'request';
 
+const JSON_TYPE = 'application/json';
+const FORM_TYPE = 'application/x-www-form-urlencoded';
+
+function formatBody (requestParams) {
+
+  // for now we support the fact that users sometimes already stringified the body
+  if (requestParams.headers['Content-Type'] === JSON_TYPE && typeof requestParams.body !== 'string') {
+    return JSON.stringify(requestParams.body);
+  }
+
+  if (requestParams.headers['Content-Type'] === FORM_TYPE && typeof requestParams.body !== 'string') {
+    const qs = new URLSearchParams();
+    Object
+      .entries(requestParams.body)
+      .forEach(([name, value]) => qs.set(name, value));
+    return qs.toString();
+  }
+
+  return requestParams.body;
+}
+
+function formatResponse (requestParams, response, rawBody) {
+
+  if (requestParams.headers['Accept'] === JSON_TYPE && response.headers['content-type'] === JSON_TYPE) {
+    return JSON.parse(rawBody);
+  }
+
+  if (requestParams.headers['Accept'] === FORM_TYPE && response.headers['content-type'] === FORM_TYPE) {
+    const objectResponse = {};
+    Array
+      .from(new URLSearchParams(rawBody).entries())
+      .forEach(([name, value]) => (objectResponse[name] = value));
+    return objectResponse;
+  }
+
+  return rawBody;
+}
+
 export async function request (requestParams) {
 
-  const jsonRequest = (requestParams.headers['Content-Type'] === 'application/json');
-  const jsonResponse = (requestParams.headers['Accept'] === 'application/json');
-  const bodyIsString = (typeof requestParams.body === 'string');
-
-  const json = !bodyIsString && (jsonRequest || jsonResponse);
+  const body = formatBody(requestParams);
 
   const options = {
     url: requestParams.url,
     method: requestParams.method,
     headers: requestParams.headers,
     qs: requestParams.queryParams,
-    body: requestParams.body,
-    json,
+    body,
+    json: false,
   };
 
   return new Promise((resolve, reject) => {
-    req(options, (error, response, resBody) => {
-      if (response.statusCode >= 400) {
-        return reject(Error(resBody.message));
+    req(options, (error, response, rawBody) => {
+      if (error != null) {
+        return reject(error);
       }
-      return (error != null) ? reject(error) : resolve(resBody);
+      const responseBody = formatResponse(requestParams, response, rawBody);
+      if (response.statusCode >= 400) {
+        return reject(Error(responseBody.message));
+      }
+      return resolve(responseBody);
     });
   });
 }
