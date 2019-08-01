57faffa08b03c729be615adfeb48e9f32552ec54
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2019-08-01 15:48:31 +0200

    Remove legacy getAuthorization

--- esm/legacy-client.node.js
+++ esm/legacy-client.node.js
@@ -30,21 +30,5 @@ export function initCleverClient (config) {
     return ownerId != null && ownerId.includes('orga_') ? legacyClient.organisations._ : legacyClient.self;
   };
 
-  // add session.getHMACAuthorization(httpMethod, url, params, tokens)
-  // so CLI can use that to sign calls to WS (events, logs, notifs...)
-  legacyClient.session = {
-    getHMACAuthorization (method, url, queryParams, tokens) {
-
-      if (!tokens.user_oauth_token || !tokens.user_oauth_token_secret) {
-        return '';
-      }
-
-      const requestParams = { method, url, queryParams };
-      const { headers } = addOauthHeader(config)(requestParams);
-
-      return headers.Authorization;
-    },
-  };
-
   return legacyClient;
 }
