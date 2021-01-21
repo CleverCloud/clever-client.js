1d58803ce2b938acc5af79c06223e3892bbdd78e
Arnaud Lefebvre <a.lefebvre@outlook.fr>
2021-01-21 18:15:33 +0100

    PickNonNull: return empty object if source object is null

--- esm/pick-non-null.js
+++ esm/pick-non-null.js
@@ -2,11 +2,13 @@ export function pickNonNull (src, keyNames) {
 
   const result = {};
 
-  Object.entries(src).forEach(([key, value]) => {
-    if (value != null && keyNames.includes(key)) {
-      result[key] = value;
-    }
-  });
+  if (src != null) {
+    Object.entries(src).forEach(([key, value]) => {
+      if (value != null && keyNames.includes(key)) {
+        result[key] = value;
+      }
+    });
+  }
 
   return result;
 }
