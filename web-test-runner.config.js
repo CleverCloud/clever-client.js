c1223d23041ef3a9c9c1b6d09ff30b84b37acfa7
Florian Sanders <florian.sanders@clever-cloud.com>
2024-10-01 20:21:32 +0200

    chore(web-test-runner): upgrade & use mocha like reporter

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,7 +1,15 @@
+import { defaultReporter, summaryReporter } from '@web/test-runner';
+
 export default {
   files: [
     'test/**/*.spec.js',
     '!test/application-logs.spec.js',
   ],
   nodeResolve: true,
+  reporters: [
+    // mocha like report
+    process.env.CI !== 'true' ? summaryReporter({ flatten: false }) : [],
+    // report global tests progress
+    defaultReporter({ reportTestResults: true, reportTestProgress: true }),
+  ],
 };
