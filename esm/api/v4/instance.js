38dc9aec03c575174877df34a63a30cbfb5febad
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2025-02-05 16:10:41 +0100

    feat: add `instance.getAllApplicationInstances`

--- esm/api/v4/instance.js
+++ esm/api/v4/instance.js
@@ -9,6 +9,30 @@ import { pickNonNull } from '../../pick-non-null.js';
  * @param {String} params.until
  * @param {String} params.includeState
  * @param {String} params.excludeState
+ * @param {String} params.deploymentId
+ * @param {String} params.limit
+ * @param {String} params.order
+ */
+export function getAllApplicationInstances(params) {
+  // no multipath for /self or /organisations/{id}
+  return Promise.resolve({
+    method: 'get',
+    url: `/v4/orchestration/organisations/${params.ownerId}/applications/${params.applicationId}/instances`,
+    headers: { Accept: 'application/json' },
+    queryParams: pickNonNull(params, ['since', 'until', 'includeState', 'excludeState', 'deploymentId', 'limit', 'order']),
+    // no body
+  });
+}
+
+/**
+ * GET /orchestration/organisations/{ownerId}/applications/{applicationId}/instances
+ * @param {Object} params
+ * @param {String} params.ownerId
+ * @param {String} params.applicationId
+ * @param {String} params.since
+ * @param {String} params.until
+ * @param {String} params.includeState
+ * @param {String} params.excludeState
  * @param {String} params.undefined
  * @param {String} params.limit
  * @param {String} params.order
