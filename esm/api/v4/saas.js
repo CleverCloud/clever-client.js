7776ecea45f24c7cfbc57310d00ccd33fb70f9e2
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2025-02-05 16:24:41 +0100

    feat: add `saas.deleteGrafanaOrganisation`

--- esm/api/v4/saas.js
+++ esm/api/v4/saas.js
@@ -1,6 +1,21 @@
 import { pickNonNull } from '../../pick-non-null.js';
 
 /**
+ * DELETE /saas/grafana/{id}
+ * @param {Object} params
+ */
+export function deleteGrafanaOrganisation() {
+  // no multipath for /self or /organisations/{id}
+  return Promise.resolve({
+    method: 'delete',
+    url: `/v4/saas/grafana/${params.id}`,
+    headers: { Accept: 'application/json' },
+    // no query params
+    // no body
+  });
+}
+
+/**
  * GET /saas/grafana/{id}
  * @param {Object} params
  * @param {String} params.id
