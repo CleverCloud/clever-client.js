66f16b54beef004b19cbdd8af43853093626d46e
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2025-02-05 16:23:36 +0100

    feat: add `saas.createGrafanaOrganisation`

--- esm/api/v4/saas.js
+++ esm/api/v4/saas.js
@@ -3,12 +3,27 @@ import { pickNonNull } from '../../pick-non-null.js';
 /**
  * GET /saas/grafana/{id}
  * @param {Object} params
- * @param {String} params.zone_id
+ * @param {String} params.id
  */
 export function getGrafanaOrganisation(params) {
   // no multipath for /self or /organisations/{id}
   return Promise.resolve({
     method: 'get',
+    url: `/v4/saas/grafana/${params.id}`,
+    headers: { Accept: 'application/json' },
+    // no query params
+    // no body
+  });
+}
+
+/**
+ * POST /saas/grafana/{id}
+ * @param {Object} params
+ */
+export function createGrafanaOrganisation() {
+  // no multipath for /self or /organisations/{id}
+  return Promise.resolve({
+    method: 'post',
     url: `/v4/saas/grafana/${params.id}`,
     headers: { Accept: 'application/json' },
     // no query params
