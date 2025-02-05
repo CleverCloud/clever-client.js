b6ee12a40f479bd9be5fb84540dc37d0b5ff7e40
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2025-02-05 15:11:41 +0100

    feat: add `providers.getEsOptionsFlavors`

--- esm/api/v2/providers.js
+++ esm/api/v2/providers.js
@@ -1,6 +1,21 @@
 import { pickNonNull } from '../../pick-non-null.js';
 
 /**
+ * GET /providers/es-addon/tmp/services-flavors
+ * @param {Object} params
+ */
+export function getEsOptionsFlavors() {
+  // no multipath for /self or /organisations/{id}
+  return Promise.resolve({
+    method: 'get',
+    url: `/v2/providers/es-addon/tmp/services-flavors`,
+    headers: { Accept: 'application/json' },
+    // no query params
+    // no body
+  });
+}
+
+/**
  * GET /providers/{providerId}/{addonId}
  * @param {Object} params
  * @param {String} params.providerId
