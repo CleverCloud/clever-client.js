255db8dfdf553de5ebedf5cab6e02f01c23657d5
Robert Tran <robert.tran@clever-cloud.com>
2025-04-04 10:02:10 +0200

    feat(zones): add `ownerId` query param
    Closes #141

--- esm/api/v4/product.js
+++ esm/api/v4/product.js
@@ -1,3 +1,5 @@
+import { pickNonNull } from '../../pick-non-null.js';
+
 /**
  * @typedef {import('../../request.types.js').RequestParams} RequestParams
  */
@@ -19,15 +21,17 @@ export function getAllZones() {
 
 /**
  * GET /products/zones
+ * @param {Object} params
+ * @param {String} params.ownerId
  * @returns {Promise<RequestParams>}
  */
-export function listZones() {
+export function listZones(params) {
   // no multipath for /self or /organisations/{id}
   return Promise.resolve({
     method: 'get',
     url: `/v4/products/zones`,
     headers: { Accept: 'application/json' },
-    // no query params
+    queryParams: pickNonNull(params, ['ownerId']),
     // no body
   });
 }
