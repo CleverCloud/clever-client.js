eb6b609686027f5b1eafa07d417f10ec4c1181bb
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2025-04-28 17:18:04 +0200

    test: add mock-api test tool

---
+++ test/lib/mock-api/mock-api.types.d.ts
@@ -0,0 +1,24 @@
+export interface Mock {
+  request: MockRequest;
+  response: MockResponse;
+}
+
+export interface MockRequest {
+  method: string;
+  path: string;
+}
+
+export interface MockResponse {
+  status: number;
+  body?: any;
+}
+
+export interface MockCall<T = any> {
+  method: string;
+  path: string;
+  queryParams: Record<string, OneOrMany<string>>;
+  headers: Record<string, OneOrMany<string>>;
+  body: T;
+}
+
+export type OneOrMany<T> = T | Array<T>;
