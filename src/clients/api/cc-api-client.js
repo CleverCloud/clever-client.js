dd37eafd6304c0691c06403eec695f0c834dc291
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2025-04-30 11:07:27 +0200

    feat: implement cc-api client

---
+++ src/clients/api/cc-api-client.js
@@ -0,0 +1,55 @@
+/**
+ * @import { CcApiCompositeCommand, CcApiSimpleCommand } from './lib/cc-api-command.js'
+ * @import { CcApiType, CcApiClientConfig } from './types/cc-api.types.js'
+ * @import { CcRequestConfig } from '../../types/request.types.js'
+ */
+import { CcClient } from '../../lib/cc-client.js';
+import { ResourceIdResolver } from './lib/resource-id-resolver.js';
+import { MemoryStore } from './lib/store/memory-store.js';
+
+/**
+ * @extends {CcClient<CcApiType>}
+ */
+export class CcApiClient extends CcClient {
+  /** @type {ResourceIdResolver} */
+  #resourceIdResolver;
+
+  /**
+   * @param {CcApiClientConfig} [config]
+   */
+  constructor(config) {
+    super({
+      baseUrl: 'https://api.clever-cloud.com',
+      ...(config ?? {}),
+    });
+
+    this.#resourceIdResolver = new ResourceIdResolver(this, config.resourceIdResolverStore ?? new MemoryStore());
+  }
+
+  /**
+   * @param {CcApiSimpleCommand<?, ?> | CcApiCompositeCommand<?, ?>} command
+   * @param {Partial<CcRequestConfig>} [requestConfig]
+   * @returns {Promise<any>}
+   * @protected
+   */
+  async _transformCommandParams(command, requestConfig) {
+    /** @type {{ownerId?: string, addonId?: string}} */
+    const resolvedIds = {};
+
+    const idsToResolve = command.getIdsToResolve();
+
+    if (idsToResolve?.ownerId) {
+      resolvedIds.ownerId = await this.#resourceIdResolver.resolveOwnerId(command.params, requestConfig);
+    }
+
+    if (idsToResolve?.addonId != null) {
+      resolvedIds.addonId = await this.#resourceIdResolver.resolveAddonId(
+        command.params,
+        idsToResolve.addonId,
+        requestConfig,
+      );
+    }
+
+    return { ...command.params, ...resolvedIds };
+  }
+}
