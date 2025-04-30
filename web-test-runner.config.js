620cb271e8dc2ffa92d6390fca939f8a1cd7c201
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2025-04-30 12:40:47 +0200

    test: run all tests in browser AND on Node

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,7 +1,8 @@
 import { defaultReporter, summaryReporter } from '@web/test-runner';
+import { mockApiPlugin } from './test/lib/mock-api/support/mock-web-test-runner-plugin.js';
 
 export default {
-  files: ['test/**/*.spec.js', '!test/application-logs.spec.js'],
+  files: ['test/**/*.spec.js', '!test/node/**/*.spec.js'],
   nodeResolve: true,
   reporters: [
     // mocha like report
@@ -9,4 +10,5 @@ export default {
     // report global tests progress
     defaultReporter({ reportTestResults: true, reportTestProgress: true }),
   ],
+  plugins: [mockApiPlugin],
 };
