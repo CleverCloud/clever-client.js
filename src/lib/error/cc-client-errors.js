910be276906e4183ed5ae8ea43a7f6eefdf72003
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2025-04-30 11:06:12 +0200

    feat: implement base client library

---
+++ src/lib/error/cc-client-errors.js
@@ -0,0 +1,101 @@
+/**
+ * @import { CcRequest, CcResponse } from '../../types/request.types.js'
+ */
+
+//--
+
+export class CcClientError extends Error {
+  /** @type {string} */
+  #code;
+
+  /**
+   * @param {string} message
+   * @param {string} code
+   * @param {unknown} [cause]
+   */
+  constructor(message, code, cause) {
+    super(message);
+    this.#code = code;
+    if (cause != null) {
+      this.cause = cause;
+    }
+  }
+
+  /**
+   * @returns {string}
+   */
+  get code() {
+    return this.#code;
+  }
+}
+
+export class CcRequestError extends CcClientError {
+  /** @type {CcRequest} */
+  #request;
+
+  /**
+   * @param {string} message
+   * @param {string} code
+   * @param {CcRequest} request
+   * @param {unknown} [cause]
+   */
+  constructor(message, code, request, cause) {
+    super(message, code, cause);
+    this.#request = request;
+  }
+
+  /**
+   * @returns {CcRequest}
+   */
+  get request() {
+    return this.#request;
+  }
+}
+
+export class CcHttpError extends CcClientError {
+  /** @type {CcResponse<?>} */
+  #response;
+
+  /**
+   * @param {string} message
+   * @param {CcResponse<?>} response
+   */
+  constructor(message, response) {
+    super(message, 'HTTP_ERROR', response.request);
+    this.#response = response;
+  }
+
+  // todo: I would really like to normalize error Response body (and have a real proper ErrorBody interface)
+  //  ovd is trying to normalize that, but work is still in progress
+
+  /**
+   * @returns {CcResponse<?>|null}
+   */
+  get response() {
+    return this.#response;
+  }
+}
+
+/**
+ * @param {any} error
+ * @returns {error is CcClientError}
+ */
+export function isCcClientError(error) {
+  return error instanceof CcClientError;
+}
+
+/**
+ * @param {any} error
+ * @returns {error is CcRequestError}
+ */
+export function isCcRequestError(error) {
+  return error instanceof CcRequestError;
+}
+
+/**
+ * @param {any} error
+ * @returns {error is CcHttpError}
+ */
+export function isCcHttpError(error) {
+  return error instanceof CcHttpError;
+}
---
+++ src/lib/utils.js
@@ -0,0 +1,95 @@
+/**
+ * @param {T} obj
+ * @param {Array<K>} keys
+ * @returns {Omit<T, K>}
+ * @template {object} T
+ * @template {keyof T} K
+ */
+export function omit(obj, ...keys) {
+  const result = { ...obj };
+  keys.forEach((key) => delete result[key]);
+  return result;
+}
+
+/**
+ * @param {T | Array<T>} value
+ * @returns {Array<T>}
+ * @template T
+ */
+export function toArray(value) {
+  return Array.isArray(value) ? value : [value];
+}
+
+/**
+ * @param {Date|string|number} date
+ * @return {string|null}
+ */
+export function normalizeOutputDate(date) {
+  if (date == null) {
+    return null;
+  }
+  if (date instanceof Date) {
+    return date.toISOString();
+  }
+  if (typeof date === 'string') {
+    return date;
+  }
+  if (typeof date === 'number') {
+    return new Date(date).toISOString();
+  }
+
+  throw new Error(`Invalid date: ${date}`);
+}
+
+/**
+ * @param {Date|string|number} date
+ * @return {string|null}
+ */
+export function normalizeInputDate(date) {
+  if (date == null) {
+    return null;
+  }
+  if (date instanceof Date) {
+    return date.toISOString();
+  }
+  if (typeof date === 'string' || typeof date === 'number') {
+    return new Date(date).toISOString();
+  }
+
+  throw new Error(`Invalid date: ${date}`);
+}
+
+/**
+ * Create a safe URL where every value is automatically encoded with the `encodeURIComponent()` function.
+ *
+ * Note that only string values are encoded. Other types are just converted to string.
+ *
+ * @param {TemplateStringsArray} strings
+ * @param {Array<?>} values
+ * @return {string}
+ */
+export function safeUrl(strings, ...values) {
+  let result = '';
+
+  strings.forEach((string, index) => {
+    result += string + encode(values[index]);
+  });
+
+  return result;
+}
+
+/**
+ * @param {?|null} value
+ * @return {string}
+ */
+function encode(value) {
+  if (value == null) {
+    return '';
+  }
+
+  if (typeof value === 'string') {
+    return encodeURIComponent(value);
+  }
+
+  return String(value);
+}
