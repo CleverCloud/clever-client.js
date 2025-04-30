64738ba9831db42ab1fc64f3dfd4ceba7945e700
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2025-04-30 11:06:12 +0200

    feat: implement base client library

---
+++ src/lib/request/request-debug.js
@@ -0,0 +1,56 @@
+/**
+ * @import {RequestWrapper, DebugFunction} from '../../types/request.types.js'
+ */
+
+import { omit } from '../utils.js';
+
+/** @type {DebugFunction} */
+const defaultDebug = (request, response, duration) => {
+  console.log(
+    JSON.stringify(
+      {
+        request: {
+          ...request,
+          queryParams: request.queryParams?.toObject(),
+          headers: obfuscateHeaders(request.headers),
+        },
+        response: {
+          ...omit(response, 'request'),
+          headers: obfuscateHeaders(response.headers),
+        },
+        duration,
+      },
+      null,
+      2,
+    ),
+  );
+};
+
+/**
+ * @type {RequestWrapper}
+ */
+export const requestDebug = async (request, handler) => {
+  if (request.debug === false) {
+    return handler(request);
+  }
+
+  const debug = request.debug === true ? defaultDebug : request.debug;
+  const now = new Date().getTime();
+  const response = await handler(request);
+  debug(request, response, new Date().getTime() - now);
+
+  return response;
+};
+
+/**
+ * @param {Headers} headers
+ */
+function obfuscateHeaders(headers) {
+  if (headers == null) {
+    return null;
+  }
+  if (headers.get('authorization') != null) {
+    return { ...Object.fromEntries(headers.entries()), authorization: '***' };
+  }
+  return Object.fromEntries(headers.entries());
+}
