46f4ec60e955b06995bd08f4207029cb6b20820b
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2025-05-02 09:58:04 +0200

    fix: fix some types

--- esm/streams/access-logs.types.d.ts
+++ esm/streams/access-logs.types.d.ts
@@ -6,17 +6,17 @@ export interface ApplicationAccessLogsStreamParams extends ApplicationAccessLogS
   tokens: OAuthTokens;
   ownerId: string;
   appId: string;
-  retryConfiguration: RetryConfiguration;
-  connectionTimeout: number;
+  retryConfiguration?: RetryConfiguration;
+  connectionTimeout?: number;
 }
 
 export interface ApplicationAccessLogStreamOptions {
-  since: Date;
-  until: Date;
-  limit: number;
-  field: Array<string>;
-  throttleElements: number;
-  throttlePerInMilliseconds: number;
+  since?: Date;
+  until?: Date;
+  limit?: number;
+  field?: Array<string>;
+  throttleElements?: number;
+  throttlePerInMilliseconds?: number;
 }
 
 export interface ApplicationAccessLog {
